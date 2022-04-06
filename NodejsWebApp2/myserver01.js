ac66409bc50db22b75c95be16999c917d2842783
Joseph <pkujm951@gmail.com>
2022-04-06 16:29:52 +0800

    新增myserver01.js

---
+++ NodejsWebApp2/myserver01.js
@@ -0,0 +1,9 @@
+﻿let http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'content-type': 'text/html;charset=utf-8' });
+    res.end('<h1>歡迎光臨!!!!</h1>');
+})
+
+    .listen(1337, function () {
+        console.log(`Web server listening on port 1337...`);
+    });
--- NodejsWebApp2/server.js
+++ NodejsWebApp2/server.js
@@ -1,8 +1,13 @@
-'use strict';
+﻿'use strict';
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+    res.end('<h1>Hello World 歡迎光臨</h1>');
+});
+
+
+server.listen(port, function () {
+    console.log(`server listening on port ${port}...`);
+});
