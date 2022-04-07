2894d30dd56428a002b34bc4b59530f1d40d380f
Joseph <pkujm951@gmail.com>
2022-04-07 11:41:19 +0800

    express 入門

---
+++ NodejsWebApp2/myExpressServer.js
@@ -0,0 +1,19 @@
+﻿let express = require('express');
+let app = express();
+app.listen(1337, function () {
+    console.log(`Express Server listening on port 1337...`);
+});
+
+app.get("/aaa", function (req, res, next) {
+    res.send("<h1>Hello world 歡迎光臨</h1>");
+    //res.end();
+    next();
+},
+    function (req, res, next) {
+        console.log("aaaaa");
+        next();
+});
+app.all("/aaa", function (req, res, next) {
+    ///res.send("<h1>您好</h1>");
+    console.log("bbbbbbbb");
+});
