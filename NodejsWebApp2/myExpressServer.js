e6a9d1a52da766e3553866a5799faa0e65ccff55
Joseph <pkujm951@gmail.com>
2022-04-07 16:31:44 +0800

    ejs

--- NodejsWebApp2/myExpressServer.js
+++ NodejsWebApp2/myExpressServer.js
@@ -4,7 +4,21 @@ app.listen(1337, function () {
     console.log(`Express Server listening on port 1337...`);
 });
 
-app.use("/",express.static(__dirname+'/public'));
+app.use("/", express.static(__dirname + '/public'));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get('/template1',function(req, res, next){
+
+    res.render("template1.ejs", { title: 'Long Time No See!!!', today: '2022/04/07', nums:[10,20,30] });
+});
+
+
+
+
+
+
+
 
 app.get("/aaa", function (req, res, next) {
     res.send("<h1>Hello world 歡迎光臨</h1>");
