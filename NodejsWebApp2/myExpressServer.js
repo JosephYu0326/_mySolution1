7741b0b3d4b8d305130bd4461fa2d1b8ded5202b
Joseph <pkujm951@gmail.com>
2022-04-12 11:52:59 +0800

    get 登入

--- NodejsWebApp2/myExpressServer.js
+++ NodejsWebApp2/myExpressServer.js
@@ -8,9 +8,27 @@ app.use("/", express.static(__dirname + '/public'));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get('/template1',function(req, res, next){
+app.get("/login", function (req, res, next) {
+    if (req.query.username == '捏小倩' && req.query.password == '令采臣') {
+        res.render("template1.ejs", {
+            title: `歡迎${req.query.username}`, today: new Date().toString(),
+            nums: []
 
-    res.render("template1.ejs", { title: 'Long Time No See!!!', today: '2022/04/07', nums:[10,20,30] });
+        });
+    }
+    else {
+        res.redirect("t07-login-form.html");
+    }
+})
+
+
+
+
+
+
+app.get('/template1', function (req, res, next) {
+
+    res.render("template1.ejs", { title: 'Long Time No See!!!', today: '2022/04/07', nums: [10, 20, 30], total: 0 });
 });
 
 
@@ -28,7 +46,7 @@ app.get("/aaa", function (req, res, next) {
     function (req, res, next) {
         console.log("aaaaa");
         next();
-});
+    });
 app.all("/aaa", function (req, res, next) {
     ///res.send("<h1>您好</h1>");
     console.log("bbbbbbbb");
